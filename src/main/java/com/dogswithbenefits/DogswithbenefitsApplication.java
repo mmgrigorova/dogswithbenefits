86158131994937674bd145682c15c17356d966aa
Maria Grigorova <mmgrigorova@gmail.com>
2018-07-23 17:50:13 +0300

    Add initial project structure

---
+++ src/main/java/com/dogswithbenefits/DogswithbenefitsApplication.java
@@ -0,0 +1,12 @@
+package com.dogswithbenefits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogswithbenefitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogswithbenefitsApplication.class, args);
+    }
+}

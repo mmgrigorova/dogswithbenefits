a27c55afbb9d9a58862b81beff34117e37972143
Maria Grigorova <mmgrigorova@gmail.com>
2018-07-26 17:59:35 +0300

    Dogs getAll()

--- src/main/java/com/antman/dogswithbenefits/web/DogRestController.java
+++ src/main/java/com/antman/dogswithbenefits/web/DogRestController.java
@@ -3,26 +3,26 @@ package com.antman.dogswithbenefits.web;
 import com.antman.dogswithbenefits.models.Dog;
 import com.antman.dogswithbenefits.services.base.DogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/dogs")
-public class DogController {
+@RequestMapping("/api")
+public class DogRestController {
     private DogService service;
 
     @Autowired
-    public DogController(DogService service) {
+    public DogRestController(DogService service) {
         this.service = service;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String getAllDogs(Model model){
-        model.addAttribute("dogs", service.getAllDogs());
-        return "listDog";
+    @GetMapping("/")
+    public List<Dog> getAllDogs(){
+        return service.getAllDogs();
     }
+
+
 }

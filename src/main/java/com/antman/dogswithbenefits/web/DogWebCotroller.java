5e19efce674dd715dee43b01850df78e8687b900
Maria Grigorova <mmgrigorova@gmail.com>
2018-07-27 16:09:43 +0300

    Added thymeleaf doglist

---
+++ src/main/java/com/antman/dogswithbenefits/web/DogWebCotroller.java
@@ -0,0 +1,27 @@
+package com.antman.dogswithbenefits.web;
+
+import com.antman.dogswithbenefits.services.base.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/dogs")
+public class DogWebCotroller {
+    private DogService service;
+
+    @Autowired
+    public DogWebCotroller(DogService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/list")
+    public ModelAndView listDogs(){
+        ModelAndView mav = new ModelAndView("dogs");
+        mav.addObject("dogs", service.getAllDogs());
+
+        return mav;
+    }
+}

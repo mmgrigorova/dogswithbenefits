4a093228168e9909e1653f800d619a0e31c8603b
Maria Grigorova <mmgrigorova@gmail.com>
2018-08-03 17:02:02 +0300

    Web layout basics

--- src/main/java/com/antman/dogswithbenefits/web/HomeController.java
+++ src/main/java/com/antman/dogswithbenefits/web/HomeController.java
@@ -1,6 +1,7 @@
 package com.antman.dogswithbenefits.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/index")
 public class HomeController {
     @GetMapping("")
-    public String indexHandler(){
+    public String indexHandler(Model model){
+        model.addAttribute("title", "Dogs with Benefits");
         return "index";
     }
 }
